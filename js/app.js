f8e34c530ab5382bfa2b74efb412110f6560924d
Abdurrahman Idris <eng-Abdelrahman.Idrees1520@alexu.edu.eg>
2021-03-23 00:59:04 +0200

    first commit, I created the navigation bar. Sections inside it will be created automatically using JS code and the width of each element inside it will also be calculated.

--- js/app.js
+++ js/app.js
@@ -17,8 +17,10 @@
  * Define Global Variables
  * 
 */
-
-
+const sectionsList = document.querySelectorAll('section');
+const navBarList = document.getElementById("navbar__list");
+const numOfSections = sectionsList.length //I'll use this variable to calculate the width of the li element style
+const sectionWidth = (100 / numOfSections);
 /**
  * End Global Variables
  * Start Helper Functions
@@ -34,7 +36,24 @@
 */
 
 // build the nav
+for (let index = 1; index <= sectionsList.length; index++){
+  const newListItem = document.createElement('li');
+  const newAnchor = document.createElement('a');
+  const sectionTitle = `Section ${index}`;
+  newAnchor.href = `#section${index}`;
+  newAnchor.innerText = sectionTitle;
+  newListItem.appendChild(newAnchor);
+  navBarList.appendChild(newListItem);
+};
 
+//changing the style.width of each li element automatically
+const liArr = document.getElementsByTagName("LI");
+for(let i = 0; i < liArr.length; i++) {
+  const parent = liArr[i].parentNode;
+  if(parent != null && parent.id == "navbar__list") {
+    liArr[i].style.width = sectionWidth + "%";
+  };        
+};
 
 // Add class 'active' to section when near top of viewport
 
